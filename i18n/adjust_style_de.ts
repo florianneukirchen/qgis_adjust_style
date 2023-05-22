991f2f9f3a8d74c322f436dce8b3dda2a60cf2b5
Florian Neukirchen <mail@riannek.de>
2023-05-22 18:17:51 +0200

    Update German translation

--- i18n/adjust_style_de.ts
+++ i18n/adjust_style_de.ts
@@ -14,79 +14,94 @@
         <translation>Stil anpassen</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="300"/>
+        <location filename="../adjust_style.py" line="336"/>
         <source>Save Styles</source>
         <translation>Stile speichern</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="293"/>
+        <location filename="../adjust_style.py" line="298"/>
         <source>Can&apos;t write to directory {}</source>
         <translation>Kann nicht in Order {} schreiben</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="295"/>
+        <location filename="../adjust_style.py" line="300"/>
         <source>Save styles to: {}</source>
         <translation>Speichere stile in: {}</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="300"/>
+        <location filename="../adjust_style.py" line="336"/>
         <source>Succesfully saved styles of {} selected layers.</source>
         <translation>Stile von {} gewählten Ebenen erfolgreich gespeichert.</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="302"/>
+        <location filename="../adjust_style.py" line="338"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="302"/>
+        <location filename="../adjust_style.py" line="338"/>
         <source>Could not save styles.</source>
         <translation>Kann Stile nicht speichern.</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="314"/>
+        <location filename="../adjust_style.py" line="354"/>
         <source>Load Styles</source>
         <translation>Stile laden</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="314"/>
+        <location filename="../adjust_style.py" line="354"/>
         <source>Can&apos;t read directory {}</source>
         <translation>Kann Order {} nicht lesen</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="317"/>
+        <location filename="../adjust_style.py" line="357"/>
         <source>Load styles from: {}</source>
         <translation>Lade Stile von: {}</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="326"/>
+        <location filename="../adjust_style.py" line="380"/>
         <source>Load styles</source>
         <translation>Lade Stile</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="322"/>
+        <location filename="../adjust_style.py" line="376"/>
         <source>Could not load any style for any layer.</source>
         <translation>Konnte für keine einzige Ebene den Stil laden.</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="326"/>
+        <location filename="../adjust_style.py" line="380"/>
         <source>Succesfully loaded styles of {} layers but failed on {} selected layers.</source>
         <translation>Stile von {} Ebenen geladen, aber kein Erfolg bei {} gewählten Ebenen.</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="762"/>
+        <location filename="../adjust_style.py" line="955"/>
         <source>File exists</source>
         <translation>Datei existiert</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="762"/>
+        <location filename="../adjust_style.py" line="955"/>
         <source>File {} already exists. Do you want to overwrite it?</source>
         <translation>Datei {} existiert bereits. Möchten Sie diese überschreiben?</translation>
     </message>
     <message>
-        <location filename="../adjust_style.py" line="780"/>
+        <location filename="../adjust_style.py" line="973"/>
         <source>Save Style {} failed:</source>
         <translation>Stil von {} konnte nicht gespeichert werden:</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style.py" line="309"/>
+        <source>Canvas background color has been saved.</source>
+        <translation>Hintergrundfarbe des Projekts wurde gespeichert.</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style.py" line="319"/>
+        <source>File backgroundcolor.txt already exists. Do you want to overwrite it?</source>
+        <translation>Die Datei backgroundcolor.txt existiert bereits. Wollen Sie diese überschreiben?</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style.py" line="1051"/>
+        <source>Preview of rotating color hue (color wheel)</source>
+        <translation>Vorschau für die Rotation des Farbtons (Farbkreis drehen)</translation>
     </message>
 </context>
 <context>
@@ -97,79 +112,144 @@
         <translation>Stil anpassen </translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="26"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="29"/>
         <source>Active Layer</source>
         <translation>Aktive Ebene</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="39"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="42"/>
         <source>Selected Layers</source>
         <translation>Ausgewählte Ebenen</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="49"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="52"/>
         <source>Visible Layers</source>
         <translation>Sichtbare Ebenen</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="59"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="62"/>
         <source>All Layers</source>
         <translation>Alle Ebenen</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="89"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="102"/>
         <source>Color (HSV): rotate hue by x°</source>
         <translation>Farbe (HSV): Farbton um x° rotieren</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="134"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="156"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="145"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="167"/>
         <source>Saturation</source>
         <translation>Sättigung</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="315"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="358"/>
         <source>+</source>
         <translation>+</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="334"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="380"/>
         <source>−</source>
         <translation>−</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="197"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="225"/>
         <source>Value</source>
         <translation>Farbwert</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="234"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="268"/>
         <source>Stroke width</source>
         <translation>Liniendicke</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="296"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="336"/>
         <source>Font Size</source>
         <translation>Schriftgröße</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="354"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="400"/>
         <source>Replace Font</source>
         <translation>Schrift ersetzen</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="385"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="434"/>
         <source>Save Styles</source>
         <translation>Stile speichern</translation>
     </message>
     <message>
-        <location filename="../adjust_style_dockwidget_base.ui" line="392"/>
+        <location filename="../adjust_style_dockwidget_base.ui" line="444"/>
         <source>Load Styles</source>
         <translation>Stile laden</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="72"/>
+        <source>All Layers + Canvas Background</source>
+        <translation>Alle Ebenen + Projekt-Hintergrundfarbe</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="143"/>
+        <source>Angle for rotating color hue (color wheel)</source>
+        <translation>Winkel bei Rotation des Farbtons (des Farbkreises)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="153"/>
+        <source>Rotate color hue (color wheel)</source>
+        <translation>Farbton (bzw. Farbkreis) drehen</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="186"/>
+        <source>Increase Saturation (+ 5)</source>
+        <translation>Farbsättigung erhöhen (+5)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="202"/>
+        <source>Decrease Saturantion (−5)</source>
+        <translation>Farbsättigung verringern (−5)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="238"/>
+        <source>Increase HSV Value (+5)</source>
+        <translation>HSV-Wert (Helligkeit) erhöhen (+5)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="254"/>
+        <source>Decrease HSV Value (−5)</source>
+        <translation>HSV-Wert (Helligkeit) verringern (−5)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="287"/>
+        <source>Lines: Increase stroke width +5 %</source>
+        <translation>Liniendicke vergrößern (+ 5 %)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="309"/>
+        <source>Lines: Decrease stroke width −5 %</source>
+        <translation>Liniendicke verringern (− 5 %)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="355"/>
+        <source>Increase Font Size +5 %</source>
+        <translation>Schriftgröße vergrößern (+ 5 %)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="377"/>
+        <source>Decrease Font Size −5 %</source>
+        <translation>Schriftgröße verkleinern (− 5 %)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="431"/>
+        <source>Save styles of selected layers to a folder (as QML files)</source>
+        <translation>Stile der gewählten Ebenen in einen Ordner speichern (als QML-Dateien)</translation>
+    </message>
+    <message>
+        <location filename="../adjust_style_dockwidget_base.ui" line="441"/>
+        <source>Load styles of layers from a folder</source>
+        <translation>Stile der Ebenen aus einem Ordner laden</translation>
     </message>
 </context>
 <context>
